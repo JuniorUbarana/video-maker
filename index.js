bec5fd5dfa3d7c539df0c215bd68b93c7ae2242e
Junior Ubarana <elci.junior@gmail.com>
2020-05-11 17:55:59 -0300

    feat : add search term and prefix

---
+++ index.js
@@ -0,0 +1,23 @@
+const readline = require('readline-sync');
+
+function start() {
+    const content = {};
+
+    content.searchTerm = askAndReturnSearchTerm();
+    content.prefix = askAndReturnPrefix();
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a wikipedia search term: ');
+    }
+
+    function askAndReturnPrefix(){
+        const prefixes = ["Who is", "What is", "The histoy of"];
+        const selectedPrefixIndexes = readline.keyInSelect(prefixes, "Choose an option: ");
+        const selectedPrefixText = prefixes[selectedPrefixIndexes];
+        console.log(selectedPrefixIndexes);
+    }
+
+    console.log(content);
+}
+
+start()
